0f239780389c76e22190922d522bfbbbcabd1b54
wuzongwen <wuzongwen@live.com>
2019-02-26 17:52:59 +0800

    首次上传代码
    首次上传代码

---
+++ SignalR/App_Start/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SignalR.App_Start.Startup))]
+namespace SignalR.App_Start
+{
+    public class Startup
+    {
+        public static void ConfigureSignalR(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+
+        public static void Configuration(IAppBuilder app)
+        {
+            Startup.ConfigureSignalR(app);
+        }
+    }
+}
---
+++ SignalR/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SignalR.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Chat()
+        {
+            return View();
+        }
+
+        public ActionResult Chat1()
+        {
+            SignalR.MyHub.Show("吴宗文", "你去干啥？");
+
+            return Content("推送消息给客户端");
+        }
+    }
+}
